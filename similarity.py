fca6ec4d857c76497e1a99f32b85d021e8c626a2
bujidao1 <3218385100@qq.com>
2021-01-15 20:15:27 +0800

    master

--- similarity.py
+++ similarity.py
@@ -1,8 +1,16 @@
 import distance
 
 
-s1 = '你在干什么'
-s2 = '你在干嘛呢'
+s1 = 'I like you'
+s2 = 'I like you too'
+#数据预处理，按空格分割字符串
+def datafilter(content):
+    s = content.split(" ")
+    return s
+s1 = datafilter(s1)
+s2 = datafilter(s2)
+
+
 def jacc_distance(s1,s2):
     first = set(s1).intersection(set(s2))
     second = set(first).union(set(s2))
@@ -11,7 +19,7 @@ def jacc_distance(s1,s2):
 x = set(list(s1))
 y = set(list(s2))
 
-#print(jacc_distance(x,y))
+print(jacc_distance(x,y))
 #杰卡德系数计算
 #用于比较样本之间的相似性与差异性，系数越大相似度越高
 from sklearn.feature_extraction.text import CountVectorizer
@@ -32,7 +40,7 @@ def jacc_similarity(s1,s2):
     #计算杰卡德系数
     return 1.0*numerator / denominator
 
-#print(jacc_similarity(s1,s2))
+print(jacc_similarity(s1,s2))
 
 #TF 计算TF矩阵中两个向量的相似度，实际上就是求解两个向量夹角的余弦值
     #就是点乘积以二者的模长，公式如下 cos = a*b/|a|*|b|
@@ -49,4 +57,17 @@ def tf_similarity(s1,s2):
     #计算TF系数
     return np.dot(vectors[0],vectors[1]) / (norm(vectors[0]) * norm(vectors[1]))
 
-print(tf_similarity(s1,s2))
+#print(tf_similarity(s1,s2))
+
+#TFIDF 在词频TF的基础上再加入IDF的信息，IDF称为逆文档频率
+from sklearn.feature_extraction.text import TfidfVectorizer
+def tfidf_similarity(s1,s2):
+    #加入空格
+    s1, s2 =add_space(s1), add_space(s2)
+    #转化为TF矩阵
+    cv = TfidfVectorizer(tokenizer=lambda  s:s.split())
+    corpus = [s1,s2]
+    vectors = cv.fit_transform(corpus).toarray()
+    #计算TFIDF系数
+    return np.dot(vectors[0], vectors[1]) / (norm(vectors[0]) * norm(vectors[1]))
+print(tfidf_similarity(s1,s2))
